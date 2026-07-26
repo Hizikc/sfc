a7df1ab3d9159fa6e71f63e87bd37591062229ec
hiz <kuznecovmatvej717@gmail.com>
2026-07-26 11:09:51 +0500

    Подключил страницы в header, добавил кнопки на станицу links без ссылок, FFT v0.0.12

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
-export default defineConfig({
+
+  export default defineConfig({
+    plugins: [
+      injectHTML() // <--- Добавляем плагин сюда
+    ],
   server: {
     host: '127.0.0.1',
     port: 3000,
